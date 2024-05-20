3331f488601c2c0b6ba59db5aa850520dfc77f83
Bryan Fullam <bryanfullam@gmail.com>
2024-05-20 16:04:59 +0200

    Prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,21 +25,8 @@ export default function RootLayout({
           <div className="flex flex-row justify-between items-center border border-b border-gray-300">
             <div className="flex flex-row items-center pl-10 p-3 space-x-5 ">
               <div className="flex flex-row items-center space-x-1">
-                <Image
-                  src="/flames.jpg" // The path to your image
-                  alt="Description of the image" // Alternative text for the image
-                  width={40} // Desired width of the image (can be changed)
-                  height={20} // Desired height of the image (can be changed)
-                />
-                <div className="text-lg font-semibold">Framefluencer</div>
+                <div className="text-lg font-semibold">Framefluence</div>
               </div>
-
-              {/* <Link href="/" passHref>
-                <span className="font-semibold pl-5">Create</span>{" "}
-              </Link>
-              <Link href="/manage" passHref>
-                <span className="font-semibold">Manage</span>{" "}
-              </Link> */}
             </div>
 
             <div>
