9bd6a4496aca52b3fb42950df6796b4d247acd88
Bryan Fullam <bryanfullam@gmail.com>
2024-05-20 15:34:00 +0200

    Wtf

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { sql } from '@vercel/postgres';
 import EcomForm from './components/EcomForm';
 
 export const metadata = {
@@ -7,12 +6,6 @@ description: "...",
 };
  
 export default async function Home(submitForm: any) {     
-    // Get frame data from the database
-    const {rows} = await sql`SELECT * FROM frames WHERE frameid = fbf01bba-efe7-483a-8d53-c8030b7633da`;
-    const frameData = rows[0];
-    console.log(frameData);
-
-    console.log('test');
     return (
         <EcomForm/>
     );
